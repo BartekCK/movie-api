57e082eba37c4e408ea97780c61abc050296ba29
Bartlomiej Kotarski <bkotarski1@gmail.com>
2021-02-20 18:50:03 +0100

    Doc start

--- src/config/routes.ts
+++ src/config/routes.ts
@@ -1,12 +1,16 @@
 import { Express } from 'express';
-
+import swaggerUi from 'swagger-ui-express';
 // routes
 import movieRouter from '../routes/movie.route';
 
 // constants
 import { API_MAIN_ROUTE } from '../constants';
 
+// json
+import swaggerDocument from '../swagger.json';
+
 const defineRouting = (server: Express): void => {
+    server.use('/docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
     server.use(`${API_MAIN_ROUTE}/movies`, movieRouter);
 };
 
